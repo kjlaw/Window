bd85e358e4953f4010b0b845fe83802a6d2cf703
Kristen Law <kristenlaw.2014@gmail.com>
2016-12-08 13:14:17 -0800

    Add profile screen

---
+++ examples/Window/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  Window
+//
+//  Created by Kristen on 12/8/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
